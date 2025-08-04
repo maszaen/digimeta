676acb9bd185412065d1b505f15ae0d090b72a08
maszaen <exqeon@gmail.com>
2025-08-04 16:27:54 +0700

    stage 3 - final batch 1 (attribute changes)

--- build.js
+++ build.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const template = fs.readFileSync(path.join(__dirname, 'env.js'), 'utf-8');
+const template = fs.readFileSync(path.join(__dirname, 'scripts', 'env.js'), 'utf-8');
 
 const output = template
   .replace('__PROJECT_ID__', process.env.PROJECT_ID)
   .replace('__ENDPOINT__', process.env.ENDPOINT)
   .replace('__BUCKET__', process.env.BUCKET);
 
-fs.writeFileSync(path.join(__dirname, 'env.js'), output);
+fs.writeFileSync(path.join(__dirname, 'scripts', 'env.js'), output);
+
+console.log('env.js berhasil dibuild!');
---
+++ env.js
@@ -0,0 +1,5 @@
+const ENV = {
+  PROJECT_ID: "688ee29d000df46e949c",
+  ENDPOINT: "https://fra.cloud.appwrite.io/v1",
+  BUCKET: "streaming"
+};
